7a2a150d5d94659a987b2e828b9dab4e3434ac26
demosuper100 <demosuper58@gmail.com>
2023-09-04 05:17:43 +0000

    Backup Branch before deleting

---
+++ README.md
@@ -0,0 +1,2 @@
+# 7a154c9c-d083-437a-868d-29f7aea52113-16953c01-a48f-4492-a7a1-2712f31df3d0
+https://sonarcloud.io/summary/overall?id=iamneo-production_7a154c9c-d083-437a-868d-29f7aea52113-16953c01-a48f-4492-a7a1-2712f31df3d0
---
+++ angularapp/src/ang.js
@@ -0,0 +1,23 @@
+console.log("ha hai")
+console.log("ha hai")
+console.log("ha hai")
+console.log("ha hai")
+console.log("ha hai")
+console.log("ha hai")
+console.log("ha hai")
+console.log("ha hai")
+
+console.log("ha hai")
+
+console.log("ha hai")
+console.log("ha hai")
+console.log("ha hai")
+console.log("ha hai")
+console.log("ha hai")
+console.log("ha hai")
+
+console.log("ha hai")
+console.log("ha hai")
+console.log("qwerty")
+console.log("ss")
+console.log("wert")
